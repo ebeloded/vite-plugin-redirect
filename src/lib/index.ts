2a60b6b36943e8303f85f5017b498e60ee736b15
Evgenij (Eugene) Beloded <belodedevgenij@gmail.com>
2022-09-06 11:41:59 +0700

    Remove immediate option

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,28 +1,19 @@
 import type { PluginOption } from 'vite';
 
-const vitePluginRedirect = (
-	redirectMap: Record<string, string>,
-	immediate: boolean = false
-): PluginOption => ({
-	name: 'redirect',
-	configureServer: (server) => {
-		const returnFunc = () =>
-			server.middlewares.use((req, res, next) => {
-				if (req.url && redirectMap[req.url] !== undefined) {
-					res.statusCode = 307;
-					res.setHeader('Location', redirectMap[req.url]);
-					res.setHeader('Content-Length', '0');
-					res.end();
-				} else {
-					next();
-				}
-			});
-
-		if (!immediate) {
-			return returnFunc;
-		}
-		returnFunc();
-	}
+const vitePluginRedirect = (redirectMap: Record<string, string>): PluginOption => ({
+  name: 'redirect',
+  configureServer: (server) => {
+    server.middlewares.use((req, res, next) => {
+      if (req.url && redirectMap[req.url] !== undefined) {
+        res.statusCode = 307;
+        res.setHeader('Location', redirectMap[req.url]);
+        res.setHeader('Content-Length', '0');
+        res.end();
+      } else {
+        next();
+      }
+    });
+  }
 });
 
 export default vitePluginRedirect;
